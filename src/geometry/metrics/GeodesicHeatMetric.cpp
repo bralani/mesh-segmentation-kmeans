8a522e2d36e5c1b0aabdf596df190afd02604dbb
Matteo Balice <matteobalice02@gmail.com>
2025-02-19 20:39:47 +0100

    heat cpp bug fix

--- src/geometry/metrics/GeodesicHeatMetric.cpp
+++ src/geometry/metrics/GeodesicHeatMetric.cpp
@@ -26,7 +26,50 @@ GeodesicHeatMetric<PT, PD>::GeodesicHeatMetric(Mesh &mesh, double percentage_thr
         }
     }
 
-    igl::heat_geodesics_precompute(V, F, data_heat);
+    const PT h = igl::avg_edge_length(V, F);
+    const PT t = h * h;
+    Eigen::SparseMatrix<PT> L, M;
+    VectorXS dblA;
+
+    igl::cotmatrix(V, F, L);
+    igl::massmatrix(V, F, igl::MASSMATRIX_TYPE_DEFAULT, M);
+    igl::doublearea(V, F, dblA);
+    igl::grad(V, F, data_heat.Grad);
+
+    assert(F.cols() == 3 && "Only triangles are supported");
+    data_heat.ng = data_heat.Grad.rows() / F.rows();
+    assert(data_heat.ng == 3 || data_heat.ng == 2);
+    data_heat.Div = -0.25 * data_heat.Grad.transpose() * dblA.colwise().replicate(data_heat.ng).asDiagonal();
+
+    Eigen::SparseMatrix<PT> Q = M - t * L;
+    Eigen::MatrixXi O;
+    igl::boundary_facets(F, O);
+    igl::unique(O, data_heat.b);
+    Eigen::SparseMatrix<PT> _;
+    auto time = std::chrono::high_resolution_clock::now();
+    if (!igl::min_quad_with_fixed_precompute(Q, Eigen::VectorXi(), _, true, data_heat.Neumann))
+    {
+        throw std::runtime_error("Error in heat_geodesics_precompute");
+        return;
+    }
+
+    // Only need if there's a boundary
+    if (data_heat.b.size() > 0)
+    {
+        if (!igl::min_quad_with_fixed_precompute(Q, data_heat.b, _, true, data_heat.Dirichlet))
+        {
+            throw std::runtime_error("Error in heat_geodesics_precompute");
+            return;
+        }
+    }
+    const Eigen::Matrix<PT, 1, Eigen::Dynamic> M_diag_tr = M.diagonal().transpose();
+    const Eigen::SparseMatrix<PT> Aeq = M_diag_tr.sparseView();
+    L *= -0.5;
+    if (!igl::min_quad_with_fixed_precompute(L, Eigen::VectorXi(), Aeq, true, data_heat.Poisson))
+    {
+        throw std::runtime_error("Error in heat_geodesics_precompute");
+        return;
+    }
 }
 
 template <typename PT, std::size_t PD>
