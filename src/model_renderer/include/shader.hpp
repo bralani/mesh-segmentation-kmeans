45d4e3047b741d2940b789ab3a6979e7223f2a45
Arcangelo Pisa <arcangelopisa@me.com>
2025-02-20 12:36:40 +0100

    Improv: add comments for shader

--- src/model_renderer/include/shader.hpp
+++ src/model_renderer/include/shader.hpp
@@ -18,65 +18,149 @@
 
 #include <plog/Log.h>
 
-class Shader;
+/**
+ * @class Shader
+ * @brief Represents an OpenGL shader.
+ *
+ * This class encapsulates the creation, compilation, and management of
+ * OpenGL shaders, such as vertex and fragment shaders.
+ */
+class Shader
+{
+    friend class ShaderProgram; ///< Allows ShaderProgram to access Shader internals.
 
-class ShaderProgram;
-
-class Shader {
-    friend ShaderProgram;
 public:
+    /**
+     * @brief Default constructor.
+     */
     Shader() {}
 
+    /**
+     * @brief Constructs a Shader from a file.
+     * @param path The file path of the shader source code.
+     * @param type The type of shader (GL_VERTEX_SHADER, GL_FRAGMENT_SHADER, etc.).
+     */
     explicit Shader(const std::string &path, GLenum type);
 
+    /**
+     * @brief Destructor. Cleans up OpenGL shader resources.
+     */
     virtual ~Shader();
 
 protected:
-    GLuint glShader;
-    GLenum glShaderType;
+    GLuint glShader;     ///< OpenGL shader ID.
+    GLenum glShaderType; ///< Type of shader (vertex, fragment, etc.).
 };
 
-class ShaderProgram {
+/**
+ * @class ShaderProgram
+ * @brief Represents an OpenGL shader program.
+ *
+ * A shader program links multiple shaders (vertex and fragment) and allows
+ * for uniform variable updates for rendering.
+ */
+class ShaderProgram
+{
 public:
+    /**
+     * @brief Default constructor.
+     */
     ShaderProgram() {}
 
+    /**
+     * @brief Constructs a ShaderProgram using precompiled shaders.
+     * @param vertex The compiled vertex shader.
+     * @param fragment The compiled fragment shader.
+     */
     ShaderProgram(const Shader &vertex, const Shader &fragment);
 
+    /**
+     * @brief Constructs a ShaderProgram from shader source files.
+     * @param vertexPath The file path to the vertex shader source.
+     * @param fragmentPath The file path to the fragment shader source.
+     */
     ShaderProgram(const std::string &vertexPath, const std::string &fragmentPath);
 
+    /**
+     * @brief Destructor. Cleans up OpenGL shader program resources.
+     */
     virtual ~ShaderProgram();
 
+    /**
+     * @brief Activates the shader program for rendering.
+     */
     void use() const;
 
-    void setInt(const std::string &name, int value) const {
+    /**
+     * @brief Sets an integer uniform variable in the shader.
+     * @param name The name of the uniform variable.
+     * @param value The integer value to set.
+     */
+    void setInt(const std::string &name, int value) const
+    {
         glUniform1i(glGetUniformLocation(glProgram, name.data()), value);
     }
 
-    void setFloat(const std::string &name, float value) const {
+    /**
+     * @brief Sets a float uniform variable in the shader.
+     * @param name The name of the uniform variable.
+     * @param value The float value to set.
+     */
+    void setFloat(const std::string &name, float value) const
+    {
         glUniform1f(glGetUniformLocation(glProgram, name.data()), value);
     }
 
-    void setVec3(const std::string &name, const glm::vec3 &value) const {
+    /**
+     * @brief Sets a vec3 uniform variable in the shader.
+     * @param name The name of the uniform variable.
+     * @param value The glm::vec3 value to set.
+     */
+    void setVec3(const std::string &name, const glm::vec3 &value) const
+    {
         glUniform3fv(glGetUniformLocation(glProgram, name.data()), 1, &value[0]);
     }
 
-    void setVec4(const std::string &name, const glm::vec4 &value) const {
+    /**
+     * @brief Sets a vec4 uniform variable in the shader.
+     * @param name The name of the uniform variable.
+     * @param value The glm::vec4 value to set.
+     */
+    void setVec4(const std::string &name, const glm::vec4 &value) const
+    {
         glUniform4fv(glGetUniformLocation(glProgram, name.data()), 1, &value[0]);
     }
 
-    void setMat3(const std::string &name, const glm::mat3 &mat) const {
+    /**
+     * @brief Sets a mat3 uniform variable in the shader.
+     * @param name The name of the uniform variable.
+     * @param mat The glm::mat3 value to set.
+     */
+    void setMat3(const std::string &name, const glm::mat3 &mat) const
+    {
         glUniformMatrix3fv(glGetUniformLocation(glProgram, name.data()), 1, GL_FALSE, &mat[0][0]);
     }
 
-    void setMat4(const std::string &name, const glm::mat4 &mat) const {
+    /**
+     * @brief Sets a mat4 uniform variable in the shader.
+     * @param name The name of the uniform variable.
+     * @param mat The glm::mat4 value to set.
+     */
+    void setMat4(const std::string &name, const glm::mat4 &mat) const
+    {
         glUniformMatrix4fv(glGetUniformLocation(glProgram, name.data()), 1, GL_FALSE, &mat[0][0]);
     }
 
+    /**
+     * @brief Sets model, view, and projection matrices in the shader.
+     * @param model The model transformation matrix.
+     * @param view The view transformation matrix.
+     * @param projection The projection transformation matrix.
+     */
     void setMVPMatrices(const glm::mat4 &model, const glm::mat4 &view, const glm::mat4 &projection);
 
 protected:
-    GLuint glProgram;
+    GLuint glProgram; ///< OpenGL shader program ID.
 };
 
-
-#endif //MESH_VIEWER_SHADER_H
+#endif // MESH_VIEWER_SHADER_H
