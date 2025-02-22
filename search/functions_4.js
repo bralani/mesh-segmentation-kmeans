b57069d3bd7575bb36123d82a18491363ecd3e2c
bralani <82759402+bralani@users.noreply.github.com>
2025-02-22 11:57:09 +0000

    Deploying to doxygen from @ AMSC-24-25/16-kmeans-16-kmeans@6f2be6d45b7e6a4cdd96963150622689c6d83bb6 🚀

--- search/functions_4.js
+++ search/functions_4.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['face_0',['Face',['../structFace.html#a34363b90a7c31d6fc4604f4e8b2a52e4',1,'Face']]],
+  ['face_0',['face',['../structFace.html#a6f6c1b69249bca94edd982ac2173c051',1,'Face::Face()=default'],['../structFace.html#a34363b90a7c31d6fc4604f4e8b2a52e4',1,'Face::Face(const std::vector&lt; VertId &gt; &amp;vertices, const std::vector&lt; Point&lt; double, 3 &gt; &gt; &amp;meshVertices, FaceId id)']]],
   ['findcentroid_1',['findcentroid',['../classCentroidInitMethod.html#ac5b8f0f0e4e4d893732f08ed8740b37f',1,'CentroidInitMethod::findCentroid()'],['../classKDE.html#aa68fd658e8f462a0d8681488d5ebee4a',1,'KDE::findCentroid()'],['../classKDE3D.html#aeec9035c2c30732fa539781304420ab0',1,'KDE3D::findCentroid()'],['../classMostDistanceClass.html#a95677d31ad2e03d6b0947c36ce4bf9c3',1,'MostDistanceClass::findCentroid()'],['../classRandomCentroidInit.html#a30f645a506514cae1f7f4b11342051c5',1,'RandomCentroidInit::findCentroid()']]],
   ['findclosestface_2',['findClosestFace',['../classGeodesicDijkstraMetric.html#ac3c887a87f2e81f550d6a74bad17db91',1,'GeodesicDijkstraMetric']]],
   ['findk_3',['findk',['../classKDEMethod.html#aa75d8dad8a4eaf36519124fe6ef13842',1,'KDEMethod::findK()'],['../classSilhouetteMethod.html#a2415282f053306e134305be27c1e7f07',1,'SilhouetteMethod::findK()'],['../classKinit.html#a882ed77d99e07f7af3da9233dd1ab5e3',1,'Kinit::findK()'],['../classElbowMethod.html#a008b8f04452f61d7c48eac9d774ec6c4',1,'ElbowMethod::findK()']]],
--- structFace.js
+++ structFace.js
@@ -1,6 +1,7 @@
 var structFace =
 [
     [ "Face", "structFace.html#a34363b90a7c31d6fc4604f4e8b2a52e4", null ],
+    [ "Face", "structFace.html#a6f6c1b69249bca94edd982ac2173c051", null ],
     [ "getArea", "structFace.html#a7c63f15147fbf57ad495d6973b9fb568", null ],
     [ "area", "structFace.html#ae63fcd017fcd355e212a2510209db17c", null ],
     [ "baricenter", "structFace.html#a0abc3357bd54d94eacaf64f103ada035", null ],
